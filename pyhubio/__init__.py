502c492cf7a45776919d6c7f7c3086189c956d34
Pavel Demin <pavel-demin@outlook.com>
2023-03-03 21:03:03 +0100

    add start and stop methods

--- pyhubio/__init__.py
+++ pyhubio/__init__.py
@@ -5,11 +5,17 @@ import time
 
 class PyhubIO:
     def __init__(self):
-        context = usb1.USBContext()
-        self.device = context.openByVendorIDAndProductID(0x0403, 0x6014)
+        self.context = None
+        self.device = None
+        self.timeout = 1000
+
+    def start(self):
+        if self.device:
+            return
+        self.context = usb1.USBContext()
+        self.device = self.context.openByVendorIDAndProductID(0x0403, 0x6014)
         if self.device is None:
             raise Exception("unable to access USB device")
-        self.timeout = 1000
         # reset mode
         self.device.controlWrite(0x40, 0x0B, 0x0000, 0x01, bytes(), self.timeout)
         # sync fifo mode
@@ -17,54 +23,69 @@ class PyhubIO:
         # latency timer
         self.device.controlWrite(0x40, 0x09, 0x00FF, 0x01, bytes(), self.timeout)
 
+    def stop(self):
+        if self.device:
+            self.context.close()
+            self.context = None
+            self.device = None
+
     def flush(self):
-        # read possible residual data
-        while len(self.device.bulkRead(0x81, 512, self.timeout)) > 2:
-            continue
-        # purge buffers
-        self.device.controlWrite(0x40, 0x00, 0x0001, 0x01, bytes(), self.timeout)
-        self.device.controlWrite(0x40, 0x00, 0x0002, 0x01, bytes(), self.timeout)
+        if self.device:
+            # read possible residual data
+            while len(self.device.bulkRead(0x81, 512, self.timeout)) > 2:
+                continue
+            # purge buffers
+            self.device.controlWrite(0x40, 0x00, 0x0001, 0x01, bytes(), self.timeout)
+            self.device.controlWrite(0x40, 0x00, 0x0002, 0x01, bytes(), self.timeout)
 
     def write(self, data, port=0, addr=0, incr=False):
-        view = data.view(np.uint8)
-        for part in np.split(view, np.arange(16384, view.size, 16384)):
-            size = part.size // 4
-            command = np.uint32((1 << 31) + ((size - 1) << 19) + (port << 16) + addr)
-            if incr:
-                addr += 4096
-            self.device.bulkWrite(0x02, command.tobytes(), self.timeout)
-            self.device.bulkWrite(0x02, part.tobytes(), self.timeout)
+        if self.device:
+            view = data.view(np.uint8)
+            for part in np.split(view, np.arange(16384, view.size, 16384)):
+                size = part.size // 4
+                command = np.uint32((1 << 31) + ((size - 1) << 19) + (port << 16) + addr)
+                if incr:
+                    addr += 4096
+                self.device.bulkWrite(0x02, command.tobytes(), self.timeout)
+                self.device.bulkWrite(0x02, part.tobytes(), self.timeout)
 
     def read(self, data, port=1, addr=0, incr=False):
-        view = data.view(np.uint8)
-        size = view.size // 4
-        div, mod = divmod(size, 4096)
-        command = np.zeros(div + (mod > 0), np.uint32)
-        for i in range(div):
-            command[i] = (4095 << 19) + (port << 16) + addr
-            if incr:
-                addr += 4096
-        if mod > 0:
-            command[-1] = ((mod - 1) << 19) + (port << 16) + addr
-        self.device.bulkWrite(0x02, command.tobytes(), self.timeout)
-        offset = 0
-        limit = size * 4
-        while offset < limit:
-            buffer = self.device.bulkRead(0x81, 16384, self.timeout)
-            buffer = np.frombuffer(buffer, np.uint8)
-            buffer = buffer[np.mod(np.arange(buffer.size), 512) > 1]
-            size = buffer.size
-            view[offset : offset + size] = buffer
-            offset += size
+        if self.device:
+            view = data.view(np.uint8)
+            size = view.size // 4
+            div, mod = divmod(size, 4096)
+            command = np.zeros(div + (mod > 0), np.uint32)
+            for i in range(div):
+                command[i] = (4095 << 19) + (port << 16) + addr
+                if incr:
+                    addr += 4096
+            if mod > 0:
+                command[-1] = ((mod - 1) << 19) + (port << 16) + addr
+            self.device.bulkWrite(0x02, command.tobytes(), self.timeout)
+            offset = 0
+            limit = size * 4
+            while offset < limit:
+                buffer = self.device.bulkRead(0x81, 16384, self.timeout)
+                buffer = np.frombuffer(buffer, np.uint8)
+                buffer = buffer[np.mod(np.arange(buffer.size), 512) > 1]
+                size = buffer.size
+                view[offset : offset + size] = buffer
+                offset += size
 
 
 class PyhubJTAG:
     def __init__(self):
-        context = usb1.USBContext()
-        self.device = context.openByVendorIDAndProductID(0x0403, 0x6010)
+        self.context = None
+        self.device = None
+        self.timeout = 1000
+
+    def start(self):
+        if self.device:
+            return
+        self.context = usb1.USBContext()
+        self.device = self.context.openByVendorIDAndProductID(0x0403, 0x6010)
         if self.device is None:
             raise Exception("unable to access USB device")
-        self.timeout = 1000
         # reset mode
         self.device.controlWrite(0x40, 0x0B, 0x0000, 0x01, bytes(), self.timeout)
         # mpsse mode
@@ -72,43 +93,53 @@ class PyhubJTAG:
         # latency timer
         self.device.controlWrite(0x40, 0x09, 0x0001, 0x01, bytes(), self.timeout)
 
+    def stop(self):
+        if self.device:
+            self.context.close()
+            self.context = None
+            self.device = None
+
     def flush(self):
-        # read possible residual command
-        while len(self.device.bulkRead(0x81, 512, self.timeout)) > 2:
-            continue
-        # purge buffers
-        self.device.controlWrite(0x40, 0x00, 0x0001, 0x01, bytes(), self.timeout)
-        self.device.controlWrite(0x40, 0x00, 0x0002, 0x01, bytes(), self.timeout)
+        if self.device:
+            # read possible residual command
+            while len(self.device.bulkRead(0x81, 512, self.timeout)) > 2:
+                continue
+            # purge buffers
+            self.device.controlWrite(0x40, 0x00, 0x0001, 0x01, bytes(), self.timeout)
+            self.device.controlWrite(0x40, 0x00, 0x0002, 0x01, bytes(), self.timeout)
 
     def setup(self):
-        buffer = np.zeros(2, np.uint8)
-        # loopback
-        command = [0x85, 0xAB]
-        self.write(np.uint8(command))
-        self.read(buffer)
-        # clock
-        command = [0x8A, 0x86, 0x00, 0x00, 0xAB]
-        self.write(np.uint8(command))
-        self.read(buffer)
-        # gpio
-        command = [0x80, 0x80, 0x8B, 0xAB]
-        self.write(np.uint8(command))
-        self.read(buffer)
+        if self.device:
+            buffer = np.zeros(2, np.uint8)
+            # loopback
+            command = [0x85, 0xAB]
+            self.write(np.uint8(command))
+            self.read(buffer)
+            # clock
+            command = [0x8A, 0x86, 0x00, 0x00, 0xAB]
+            self.write(np.uint8(command))
+            self.read(buffer)
+            # gpio
+            command = [0x80, 0x80, 0x8B, 0xAB]
+            self.write(np.uint8(command))
+            self.read(buffer)
 
     def write(self, data):
-        self.device.bulkWrite(0x02, data.tobytes(), self.timeout)
+        if self.device:
+            self.device.bulkWrite(0x02, data.tobytes(), self.timeout)
 
     def read(self, data):
-        view = data.view(np.uint8)
-        offset = 0
-        limit = view.size
-        while offset < limit:
-            buffer = self.device.bulkRead(0x81, 16384, self.timeout)
-            buffer = np.frombuffer(buffer, np.uint8)
-            buffer = buffer[np.mod(np.arange(buffer.size), 512) > 1]
-            size = buffer.size
-            view[offset : offset + size] = buffer
-            offset += size
+        if self.device:
+            view = data.view(np.uint8)
+            offset = 0
+            limit = view.size
+            while offset < limit:
+                buffer = self.device.bulkRead(0x81, 16384, self.timeout)
+                buffer = np.frombuffer(buffer, np.uint8)
+                buffer = buffer[np.mod(np.arange(buffer.size), 512) > 1]
+                size = buffer.size
+                view[offset : offset + size] = buffer
+                offset += size
 
     def tms(self, data, bits):
         command = [0x4B, bits - 1, data]
@@ -141,32 +172,36 @@ class PyhubJTAG:
         self.shift_bits(view[-1], 8)
 
     def idcode(self):
-        buffer = np.zeros(1, np.uint32)
-        self.shift_ir()
-        self.shift_bits(0x09, 6)
-        self.shift_dr()
-        command = [0x2C, 0x03, 0x00]
-        self.write(np.uint8(command))
-        self.read(buffer)
-        self.idle()
-        return buffer[0] & 0x0FFFFFFF
+        if self.device:
+            buffer = np.zeros(1, np.uint32)
+            self.shift_ir()
+            self.shift_bits(0x09, 6)
+            self.shift_dr()
+            command = [0x2C, 0x03, 0x00]
+            self.write(np.uint8(command))
+            self.read(buffer)
+            self.idle()
+            return buffer[0] & 0x0FFFFFFF
+        else:
+            return 0x0FFFFFFF
 
     def program(self, path):
-        data = np.fromfile(path, np.uint8)
-        for i in range(data.size - 4):
-            if np.array_equal(data[i : i + 4], [0xAA, 0x99, 0x55, 0x66]):
-                break
-        data = data[i:]
-        data = ((data >> 1) & 0x55) | ((data & 0x55) << 1)
-        data = ((data >> 2) & 0x33) | ((data & 0x33) << 2)
-        data = ((data >> 4) & 0x0F) | ((data & 0x0F) << 4)
-        # jprogram
-        self.shift_ir()
-        self.shift_bits(0x0B, 6)
-        self.idle()
-        time.sleep(0.01)
-        # cfg_in
-        self.shift_ir()
-        self.shift_bits(0x05, 6)
-        self.shift_dr()
-        self.shift_bytes(data)
+        if self.device:
+            data = np.fromfile(path, np.uint8)
+            for i in range(data.size - 4):
+                if np.array_equal(data[i : i + 4], [0xAA, 0x99, 0x55, 0x66]):
+                    break
+            data = data[i:]
+            data = ((data >> 1) & 0x55) | ((data & 0x55) << 1)
+            data = ((data >> 2) & 0x33) | ((data & 0x33) << 2)
+            data = ((data >> 4) & 0x0F) | ((data & 0x0F) << 4)
+            # jprogram
+            self.shift_ir()
+            self.shift_bits(0x0B, 6)
+            self.idle()
+            time.sleep(0.01)
+            # cfg_in
+            self.shift_ir()
+            self.shift_bits(0x05, 6)
+            self.shift_dr()
+            self.shift_bytes(data)
