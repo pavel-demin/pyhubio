cd2c5204822f271ff44d82feb92a5898d2f04616
Pavel Demin <pavel-demin@outlook.com>
2023-07-02 21:58:50 +0200

    add PyhubTCP

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-This library uses [python-libusb1](https://github.com/vpelletier/python-libusb1) to communicate with configuration registers, status registers, AXI4-Stream and BRAM ports via AXI4-Stream USB and AXI4-Stream hub modules:
+This library is used to communicate with configuration registers, status registers, AXI4-Stream and BRAM interfaces on Digilent USB104 A7 and Red Pitaya STEMlab boards:
+
+https://pavel-demin.github.io/usb104-a7-notes
+
+https://pavel-demin.github.io/red-pitaya-notes
+
+This library uses the [python-libusb1](https://github.com/vpelletier/python-libusb1) library to communicate via the USB interface with the AXI4-Stream USB and AXI4-Stream hub modules on the Digilent USB104 A7 board:
 
 https://pavel-demin.github.io/usb104-a7-notes/usb-interface
 
-This library is mainly used with the applications for the USB104 A7 board:
+This library uses the [socket](https://docs.python.org/3/library/socket.html) library to communicate via the Ethernet interface with the AXI4 hub module on the Red Pitaya STEMlab board:
 
-https://pavel-demin.github.io/usb104-a7-notes
+https://pavel-demin.github.io/red-pitaya-notes/axi-hub
--- pyhubio/__init__.py
+++ pyhubio/__init__.py
@@ -1,4 +1,5 @@
 import usb1
+import socket
 import numpy as np
 import time
 
@@ -244,3 +245,80 @@ class PyhubJTAG:
         self.idle()
         self.configure(path)
         self.stop()
+
+
+class PyhubTCP:
+    def __init__(self, host="192.168.1.100", port=1001):
+        self.address = (host, port)
+        self.socket = None
+
+    def start(self):
+        if self.socket:
+            return
+        self.socket = socket.create_connection(self.address, 1)
+
+    def stop(self):
+        if self.socket is None:
+            return
+        self.socket.close()
+        self.socket = None
+
+    def read(self, data, port=1, addr=0):
+        if self.socket is None:
+            return
+        view = data.view(np.uint8)
+        for part in np.split(view, np.arange(16777216, view.size, 16777216)):
+            view = part.view(np.uint8)
+            size = part.size
+            incr = np.arange(0, size, 65536, np.uint64)
+            rlen = np.full_like(incr, 65536)
+            mod = size % 65536
+            if mod > 0:
+                rlen[-1] = mod
+            command = rlen << 28 | (port & 0x7) << 24 | (addr + incr) & 0xFFFFFF
+            self.socket.sendall(command.tobytes())
+            offset = 0
+            limit = view.size
+            while offset < limit:
+                buffer = self.socket.recv(limit - offset)
+                buffer = np.frombuffer(buffer, np.uint8)
+                size = buffer.size
+                view[offset : offset + size] = buffer
+                offset += size
+            addr += part.size
+
+    def write(self, data, port=0, addr=0):
+        if self.socket is None:
+            return
+        view = data.view(np.uint8)
+        for part in np.split(view, np.arange(65536, view.size, 65536)):
+            size = part.size
+            command = np.uint64([1 << 52 | size << 28 | (port & 0x7) << 24 | addr & 0xFFFFFF])
+            addr += part.size
+            self.socket.sendall(command.tobytes())
+            self.socket.sendall(part.tobytes())
+
+    def edge(self, data, mask, positive=True, addr=0):
+        if self.socket is None:
+            return data
+        command = np.uint64([1 << 52 | 1 << 28 | addr & 0xFFFFFF])
+        view = command.view(np.uint8)
+        lo = np.uint8([data & ~mask])
+        hi = np.uint8([data | mask])
+        if positive:
+            sequence = np.concatenate((view, lo, view, hi))
+            result = hi
+        else:
+            sequence = np.concatenate((view, hi, view, lo))
+            result = lo
+        self.socket.sendall(sequence.tobytes())
+        return result
+
+    def program(self, path):
+        if self.socket is None:
+            return
+        data = np.fromfile(path, np.uint8)
+        size = data.size
+        command = np.uint64([2 << 52 | size << 28])
+        self.socket.sendall(command.tobytes())
+        self.socket.sendall(data.tobytes())
